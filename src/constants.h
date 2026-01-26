dc99cb23e8aed3389bb15a494cc6bdb9346ca3de
andreabp19 <andrea.bp.art@gmail.com>
2026-01-26 13:25:47 -0600

    Updated comments

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,7 @@
 /**
  * Library configuration constants.
  * 
- * Last modified: 26 nov. 2025
+ * Last modified: 22 Jan. 2026
  *      By: Andrea Pineda
  */
 
@@ -11,12 +11,12 @@
 // ====================================================================================================
 // Constant macro definitions
 // ====================================================================================================
-#define MAX_ITERATION_COUNT_SVD (100)    /**< Maximum number of iterations for svd_jacobi_one_sided.c */
-#define MAX_ITERATION_COUNT_SQP (30)    /**< Maximum number of iterations for quadprog_sqp */
+#define MAX_ITERATION_COUNT_SVD (100)    /**< Maximum number of iterations for svd_jacobi_one_sided.c. Reduce or increase as needed. */
+#define MAX_ITERATION_COUNT_SQP (30)    /**< Maximum number of iterations for quadprog_sqp. The algorithm will end when it converges, even if it hasn't reached the limit of iterations. */
 #define MAX_VEC_SIZE            (10)   /**< Maximum number of elements allowed for a single row vector. */
-#define MAX_MAT_SIZE            (MAX_VEC_SIZE*MAX_VEC_SIZE)     /**< Maximum number of elements allowed for a matrix. */
+#define MAX_MAT_SIZE            (MAX_VEC_SIZE*MAX_VEC_SIZE)     /**< Maximum number of elements allowed for a matrix. WARNING: Routines are tested up to 10x10 matrices, increase with care. */
 #define MATH_MATRIX_CHECK               /**< Comment this to disable matrix size checking. */
 #define MATH_EQUAL_PRECISION    (1E-5f)  /**< Precision of equal comparisons. WARNING: Algorithms may break if they can't reach specified precision. Adjust as needed.*/
-#define ILL_CONDITIONING_THRESHOLD (100.0) /** Number starting from which to consider ill-conditioning and apply SVD in linsolve */
+#define ILL_CONDITIONING_THRESHOLD (100.0) /** Condition Number starting from which linsolve will use SVD to solve linear systems */
 
 #endif // ROBOTAT_CONSTANTS_H_
