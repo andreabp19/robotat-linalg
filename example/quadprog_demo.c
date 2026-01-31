d2bed81eea7ea8317643e8101ba880a7abb82901
andreabp19 <andrea.bp.art@gmail.com>
2026-01-31 17:43:19 -0600

    Added example case for active-set method (quadprog_sqp)

--- example/quadprog_demo.c
+++ example/quadprog_demo.c
@@ -5,7 +5,7 @@
  * Last Modified 30 Dec 2025
  *            By: Andrea Pineda
  * 
- * Quadprog demo
+ * quadprog demo
  */
 
 // ---------------------------------------------------------------------------
@@ -38,18 +38,24 @@ static float m9data[MAX_MAT_SIZE];
 static matf32_t m9;
 static float m10data[MAX_MAT_SIZE];
 static matf32_t m10;
+static float m11data[MAX_MAT_SIZE];
+static matf32_t m11;
+static float m12data[MAX_MAT_SIZE];
+static matf32_t m12;
 
 matf32_t* Q = &m1;
 matf32_t* c = &m2;
 matf32_t* Aeq = &m3;
 matf32_t* beq = &m4;
-matf32_t* x = &m5;
+matf32_t* Ain = &m5;
+matf32_t* bin = &m6;
+matf32_t* x = &m7;
 
-matf32_t* Aeqx = &m6;
-matf32_t* xt = &m7;
-matf32_t* xtc = &m8;
-matf32_t* xtQ = &m9;
-matf32_t* xtQx = &m10;
+matf32_t* Aeqx = &m8;
+matf32_t* xt = &m9;
+matf32_t* xtc = &m10;
+matf32_t* xtQ = &m11;
+matf32_t* xtQx = &m12;
 
 quadprog_t qp;
 quadprog_t sqp;
@@ -58,30 +64,35 @@ int main(void)
 {
     bool tolerance_check = false;
 
-    uint16_t n = 5;
+    uint16_t n = 3;
 
     matf32_init(Q, n, n, m1data);
     matf32_init(c, n, 1, m2data);
     matf32_init(Aeq, n, n, m3data);
     matf32_init(beq, n, 1, m4data);
-    matf32_init(x, n, 1, m5data);
+    matf32_init(Ain, n, n, m5data);
+    matf32_init(bin, n, 1, m6data);
+    matf32_init(x, n, 1, m7data);
 
-    matf32_init(Aeqx, n, 1, m6data);
-    matf32_init(xt, 1, n, m7data);
-    matf32_init(xtc, xt->num_rows, c->num_cols, m8data);
-    matf32_init(xtQ, xt->num_rows, Q->num_cols, m9data);
-    matf32_init(xtQx, xt->num_rows, x->num_cols, m10data);
+    matf32_init(Aeqx, n, 1, m8data);
+    matf32_init(xt, 1, n, m9data);
+    matf32_init(xtc, xt->num_rows, c->num_cols, m10data);
+    matf32_init(xtQ, xt->num_rows, Q->num_cols, m11data);
+    matf32_init(xtQx, xt->num_rows, x->num_cols, m12data);
 
     matf32_randn(Q, 0, 1);
     matf32_randn(c, 0, 1);
     matf32_randn(Aeq, 0, 1);
     matf32_randn(beq, 0, 1);
+    matf32_randn(Ain, 0, 1);
+    matf32_randn(bin, 0, 1);
 
     // ---------------------------------------------------------------------------
     // 1. QPs with equality restrictions
     // ---------------------------------------------------------------------------
 
-    quadprog_init(&qp, Q, c, Aeq, beq, NULL, NULL, NULL); // NULL: No Ain, bin, x0
+    quadprog_init(&qp, Q, c, Aeq, beq, NULL, NULL, NULL); // Equality-restricted QP. Ain, bin and x0 = NULL (No inequality restrictions, no initial point x0)
+    quadprog_init(&sqp, Q, c, Aeq, beq, Ain, bin, NULL); // Inequality-restricted QP (to be solved with the active-set method quadprog_sqp). No initial point x0
 
     printf("\n---------------------------------------------------------------------------\n");
     printf("Matrices of the QP");
@@ -266,6 +277,22 @@ int main(void)
     printf("quadprog_sqp (active-set method)");
     printf("\n---------------------------------------------------------------------------\n");
 
-    
+    quadprog_sqp(&sqp, x);
 
+    // Calculate cost: q = (1/2)x'*Q*x + x'*c
+    matf32_trans(x, xt);
+    matf32_mul(xt, c, xtc);
+    matf32_mul(xt, Q, xtQ);
+    matf32_mul(xtQ, x, xtQx);
+    matf32_scale(xtQx, 0.5, xtQx);
+
+    printf("Cost q = (1/2)x'*Q*x + x'*c:\n");
+    matf32_print(xtQx);
+
+    matf32_mul(Aeq, x, Aeqx);
+    printf("Solution Ax of the restrictions: \n");
+    matf32_print(Aeqx);
+
+    tolerance_check = matf32_is_equal(Aeqx, beq);
+    printf("Difference of Ax and b smaller than 1E-05? %s\n", tolerance_check?"True":"False");
 }
